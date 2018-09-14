a57677b79fe35fe4a6325241413897e68b6137fa
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2018-09-14 18:07:14 +0300

    cleanup and numpy short notation

--- monte_carlo.py
+++ monte_carlo.py
@@ -13,82 +13,95 @@ Original code:
 """
 
 import matplotlib.pyplot as plt
-from numpy.random import triangular, normal
+from numpy.random import triangular
 
 
-N=100_000
+N=1_000_000
+
+# Seed effieciencies and prices
+
+DISTRIBUTION_PARAM_COP_GWP = dict(left=4, mode=4.5, right=5)
+DISTRIBUTION_PARAM_COP_HP = dict(left=35, mode=40, right=45)
+DISTRIBUTION_PARAM_PRICE = dict(left=140, mode=150, right=160)
+
+def seed_cop_hp():
+    return triangular(**DISTRIBUTION_PARAM_COP_HP)
+
+def seed_cop_gwp():
+    return triangular(**DISTRIBUTION_PARAM_COP_GWP) 
+
+def seed_price(): #(€/MWh)
+    return triangular(**DISTRIBUTION_PARAM_PRICE)
 
 # Groundwater cycle
 
 ENERGY_HEATING_MWH = 866
 ENERGY_COOLING_MWH = 912
 
-COP_DISTRIBUTION_PARAM = dict(left=4, mode=4.5, right=5)
-
-# What is missnig is the COP of the groundwater pump
-COP_GW_PUMP_DISTRIBUTION_PARAM = dict(left=35, mode=40, right=45)
-
-# I changed this to a triangular distributon. 
-PRICE_DISTRIBUTION_PARAM = dict(left=140, mode=150, right=160)
-
-def seed_cop_hp():
-    return triangular(**COP_DISTRIBUTION_PARAM)
-
-def seed_price(): #(€/MWh)
-    return triangular(**PRICE_DISTRIBUTION_PARAM) 
-
-def seed_cop_gw_pump():
-    return triangular(**COP_GW_PUMP_DISTRIBUTION_PARAM) 
 
 def gw_consumption():
-    return (ENERGY_HEATING_MWH + ENERGY_COOLING_MWH) / seed_cop_gw_pump() 
+    return (ENERGY_HEATING_MWH + ENERGY_COOLING_MWH) / seed_cop_gwp() 
 
 def gw_cost():
     """Groundwater cycle cost, euro"""
     return gw_consumption() * seed_price()
+
+# Heatpump cycle
 
 def hp_consumption():
     """Calculations of the electricty consumption of the heat pump. 
        Input: ENERGY_HEATING_MWH = 866 (pumped energy from subsurface)
        Output: energy input divided by COP of the heat pump
     """
+    # FIXME: there should be -1?
     return ENERGY_HEATING_MWH / (seed_cop_hp() - 1)
 
 def hp_cost():
     """heat pump cost, euro"""
     return hp_consumption() * seed_price()
 
-# Summation of heat pump and groundwater pump costs
 def total_costs():
+    """Summation of heat pump and groundwater pump costs"""
     return hp_cost() + gw_cost()
+
+# Utility function
 
 def sim(func, n=N):
     return [func() for _ in range(n)]
 
 def plot(x, header=""):
-    # TODO: add header to plot
     plt.hist(x, bins=75, density=True)
+    plt.title(header)
     plt.figure()
 
-# NOTE: same can be down with fewer fucntions and numpy arrays
-# With the calculation of the heat pump costs, the code is getting quite long again. 
-# It would be good to shorten it by using numpy.
+def plot_from_func():
+    prices = sim(seed_price)
+    plot(prices, "Electricity prices")
+    
+    gw_pump_costs = sim(gw_cost)
+    plot(gw_pump_costs, "Groundwater pump costs")
+    
+    heat_pump_costs = sim(hp_cost)
+    plot(heat_pump_costs, "Heatpump costs")
+    
+    total_costs_list = sim(total_costs)
+    plot(total_costs_list, "Total costs")
+
+# Numpy form (very short)
+
+def seed_triangular(param):
+    return triangular(**param, size=N)
+
+cop_gw = seed_triangular(DISTRIBUTION_PARAM_COP_GWP)
+cop_hp = seed_triangular(DISTRIBUTION_PARAM_COP_HP)
+p = seed_triangular(DISTRIBUTION_PARAM_PRICE)
+ec_gw = (ENERGY_HEATING_MWH + ENERGY_COOLING_MWH) / cop_gw
+ec_hp = ENERGY_HEATING_MWH / (cop_hp - 1)
+total_costs_np = (ec_gw + ec_hp) * p
+
+
+# NEXT:
+#   - what are parameters of 70 systems?
 
 if __name__ == "__main__":
-    # triangular
-    gw_consumption_list = sim(gw_consumption)
-    plot(gw_consumption_list)
-    # normal
-    prices = sim(seed_price)
-    plot(prices)
-    # kind of normal (triangular * normal)
-    total_cost_simulated = sim(gw_cost)
-    plot(total_cost_simulated)
-    # kind of normal (normal + normal)
-    heat_pump_costs = sim(hp_cost)
-    plot( heat_pump_costs)
-    # kind of normal (normal + normal)
-    total_costs = sim(total_costs)
-    plot(total_costs)
-
-
+    plot(total_costs_np, header=f"Total costs, {N/10**6} mln simulations")
