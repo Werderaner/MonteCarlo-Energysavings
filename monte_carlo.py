8c74a1263ba354543315531fa03c40028db71d91
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2018-09-14 17:33:26 +0300

    Update monte_carlo.py

--- monte_carlo.py
+++ monte_carlo.py
@@ -5,8 +5,7 @@ Find out electricity consumption of three cyles:
 2. groundwater pump (winter)
 3. heat pump (summer)
 
-Apply electricity price to total comsumptiom to derive
-total electricity cost.  
+Apply electricity price to total comsumptiom to derive total electricity cost.  
 
 Original code: 
     https://stackoverflow.com/questions/52258435/monte-carlo-simulation-with-triangular-and-normal-probability-density-distibutio/52260330?noredirect=1#comment91526700_52260330
@@ -23,8 +22,7 @@ N=100_000
 
 ENERGY_HEATING_MWH = 866
 ENERGY_COOLING_MWH = 912
-# FIXME: you have 40 in readme, not 4 
-# We need both. 40 is the COP of the groundwater pump, 4 of the heat pump.
+
 COP_DISTRIBUTION_PARAM = dict(left=4, mode=4.5, right=5)
 
 # What is missnig is the COP of the groundwater pump
@@ -42,9 +40,6 @@ def seed_price(): #(€/MWh)
 def seed_cop_gw_pump():
     return triangular(**COP_GW_PUMP_DISTRIBUTION_PARAM) 
 
-# QUESTION: is this right for a groundwater cycle? choose one cop for both seasons?
-# Yes, beacause we are using the same pump to extract the groundwater during winter- & summertime
-
 def gw_consumption():
     return (ENERGY_HEATING_MWH + ENERGY_COOLING_MWH) / seed_cop_gw_pump() 
 
@@ -52,9 +47,11 @@ def gw_cost():
     """Groundwater cycle cost, euro"""
     return gw_consumption() * seed_price()
 
-"""Calculations of the elec. costs of the heat pump. Input: ENERGY_HEATING_MWH = 866 (pumped energy from subsurface);
-Output: energy input divided by COP of the heat pump """
 def hp_consumption():
+    """Calculations of the electricty consumption of the heat pump. 
+       Input: ENERGY_HEATING_MWH = 866 (pumped energy from subsurface)
+       Output: energy input divided by COP of the heat pump
+    """
     return ENERGY_HEATING_MWH / (seed_cop_hp() - 1)
 
 def hp_cost():
@@ -74,8 +71,8 @@ def plot(x, header=""):
     plt.figure()
 
 # NOTE: same can be down with fewer fucntions and numpy arrays
-# With the calculation of the heat pump costs, the code is getting quite long again. It would be good to shorten it by using numpy.
-
+# With the calculation of the heat pump costs, the code is getting quite long again. 
+# It would be good to shorten it by using numpy.
 
 if __name__ == "__main__":
     # triangular
@@ -93,146 +90,5 @@ if __name__ == "__main__":
     # kind of normal (normal + normal)
     total_costs = sim(total_costs)
     plot(total_costs)
-    
-# Groundwater cycle
-
-# EP to be discussed - I do not underatand logic of energy in and energy out
-    
-
-
-
-## EP - this seems to your equations, correct?
-#
-#def energy_output(coef_performance, energy_input):
-#    return energy_input * coef_performance / (coef_performance - 1)
-#
-## FIXME: this is the same fucntion written 3 times, you need just one
-#
-#def elec_costs_heatpump(elec_costs, coef_performance, energy_output):
-#    return energy_output * 1000 / coef_performance * elec_costs
-#
-#
-#def elec_costs_coldpump(elec_costs, coef_performance_pump, energy_input):
-#    return energy_input * 1000 / coef_performance_pump * elec_costs
-#
-#
-#def elec_costs_warmpump(elec_costs, coef_performance_pump, energy_input):
-#    return energy_input * 1000 / coef_performance_pump * elec_costs
-#
-## end of FIXME
-#
-#
-#def total_costs(costs_heatpump, costs_coldpump, costs_warmpump):
-#    return costs_heatpump + costs_coldpump + costs_warmpump
-#
-#
-## This is seeding and calculation
-#
-# 
-#
-## constants and seeds
-#
-#N = 50
-#
-#
-#COP_DISTRIBUTION_PARAM = dict(left=4, mode=4.5, right=5)
-#
-#
-#def seed_cop():
-#    return triangular(**COP_DISTRIBUTION_PARAM)
-#
-#
-#
-#
-#ELEC_DISTRIBUTION_PARAM = dict(left=0.14, mode=0.15, right=0.16)
-#
-#def seed_elec():
-#    return triangular(**ELEC_DISTRIBUTION_PARAM)
-#
-#
-#COP_PUMP_DISTRIBUTION_PARAM = dict(left=35, mode=40, right=45)
-#
-#
-#def seed_cop_pump():
-#    return triangular(**COP_PUMP_DISTRIBUTION_PARAM)
-#
-#
-## chain of calulation
-#
-#def random_energy_output():
-#    return energy_output(seed_cop(), energy_input=INPUT_ENERGY_HEATING)
-#
-#
-#energy_outputs = [random_energy_output() for _ in range(N)]
-#
-#a = min(energy_outputs)
-#b = max(energy_outputs)
-#med = np.median(energy_outputs)
-#
-## Calculation of electricity costs for heat pump
-#
-#HP_OUTPUT_DISTRIBUTION_PARAM = dict(left=a, mode=med, right=b)
-#
-#
-#def seed_output():
-#    return triangular(**HP_OUTPUT_DISTRIBUTION_PARAM)
-#
-#
-#def random_elec_costs_heatpump():
-#    return elec_costs_heatpump(seed_elec(), seed_cop(), seed_output())
-#
-#
-#elec_costs_heatpump = [random_elec_costs_heatpump() for _ in range(N)]
-#mean_hp = np.mean(elec_costs_heatpump)
-#std_hp = np.std(elec_costs_heatpump)
-#
-#
-## Calculation of electricity costs for pumping of cold energy
-#
-#def random_elec_costs_coldpump():
-#    return elec_costs_coldpump(seed_elec(), seed_cop_pump(), energy_input=INPUT_ENERGY_COOLING)
-#
-#elec_costs_coldpump = [random_elec_costs_coldpump() for _ in range(N)]
-#mean_cp = np.mean(elec_costs_coldpump)
-#sdt_cp = np.std(elec_costs_coldpump)
-## Calculation of electricity costs for pumping of warm energy
-#
-#
-#def random_elec_costs_warmpump():
-#    return elec_costs_warmpump(seed_elec(), seed_cop_pump(), energy_input=INPUT_ENERGY_HEATING)
-#
-#
-#elec_costs_warmpump = [random_elec_costs_warmpump() for _ in range(N)]
-#mean_wp = np.mean(elec_costs_warmpump)
-#sdt_wp = np.std(elec_costs_warmpump)
-## Sumation of electricity costs
-#
-#
-#ELEC_COSTS_HEATPUMP_PARAM = dict(loc=mean_hp, scale=std_hp)
-#
-#
-#def seed_costs_hp():
-#    return np.random.normal(**ELEC_COSTS_HEATPUMP_PARAM)
-#
-## EP: where is the gaussiean pdf that yoyu want to replace?
-## FIXME: why does numpy does not fit?
-#ELEC_COSTS_COLDPUMP_PARAM = dict(loc=mean_cp, scale=sdt_cp)
-#
-#
-#def seed_costs_cp():
-#    return np.random.normal(**ELEC_COSTS_COLDPUMP_PARAM)
-#
-#
-#ELEC_COSTS_WARMPUMP_PARAM = dict(loc=mean_wp, scale=sdt_wp)
-#
-#
-#def seed_cost_wp():
-#    return np.random.normal(**ELEC_COSTS_WARMPUMP_PARAM)
-#
-#
-#def random_total_costs():
-#    return seed_costs_hp(), seed_costs_cp(), seed_cost_wp()
-#
-## Note the program run surprising slow for such easy cualulations.
 
 
